3ef235bbe3fc9da556fbeeba6ce4b1b1b8c841ab
Eric Sondergard <flamtap@users.noreply.github.com>
2017-07-22 21:55:24 -0600

    Add script to install chocolatey packages.

---
+++ choco.ps1
@@ -0,0 +1,44 @@
+#Install Chocolatey (https://chocolatey.org/)
+Set-ExecutionPolicy RemoteSigned
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Install Chocolatey packages
+choco install 1password -y
+choco install 7zip -y
+choco install avastfreeantivirus -y
+choco install brave -y
+choco install bulkrenameutility -y
+choco install ccleaner -y
+choco install ConEmu -y
+choco install DotNet3.5 -y
+choco install DotNet4.5 -y
+choco install DotNet4.5.2 -y
+choco install DotNet4.6 -y
+choco install DotNet4.6 -y
+choco install DotNet4.6.2 -y
+choco install dotnetcore -y
+choco install dropbox -y
+choco install Firefox -y
+choco install foxitreader -y
+choco install git -y
+choco install linqpad5 -y
+choco install nuget.commandline -y
+choco install nunit-console-runner -y
+choco install OpenOffice -y
+choco install poshgit -y
+choco install ReSharper -y
+choco install Slack -y
+choco install Spotify -y
+choco install sql-server-management-studio -y
+choco install steam -y
+choco install sublimetext3 -y
+choco install utorrent -y
+choco install VisualStudio2017Enterprise -y
+choco install VisualStudioCode -y
+choco install vlc -y
+choco install vscode-csharp -y
+choco install vscode-csharpextensions -y
+choco install vscode-mssql -y
+choco install vscode-cake -y
+choco install vscode-gitignore -y
+choco install vscode-gitattributes -y
