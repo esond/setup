906cf3b7d3ec6032432a5644c3f4505cf02ac7aa
Eric Sondergard <4650644+Flamtap@users.noreply.github.com>
2018-08-18 15:36:11 -0600

    Update choco.ps1

--- choco.ps1
+++ choco.ps1
@@ -1,38 +1,45 @@
-#Install Chocolatey (https://chocolatey.org/)
+# Requires Execution-Policy to be set to Remote-Signed
+
+# Install Chocolatey (https://chocolatey.org/)
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/upgrade.ps1'))
+refreshenv
 choco feature enable -n allowGlobalConfirmation
 
-#Install Chocolatey packages
+# Install Chocolatey packages
+cup 1Password
 cup 7zip
+cup azure-cli
 cup baretail
-cup brave
+cup bleachbit
 cup bulkrenameutility
-cup ccleaner
+cup chocolateygui
 cup ConEmu
 cup DotNet4.5.2
 cup DotNet4.6
 cup DotNet4.6.2
 cup dotnet4.7
 cup dotnet4.7.1
+cup dotnet4.7.2
 cup dotnetcore
 cup dotnetcore-sdk
-cup dropbox
 cup Firefox
-cup foxitreader
 cup git
+cup GoogleChrome
 cup grepwin
 cup iisexpress
+cup iTunes
 cup linqpad5
 cup nuget.commandline
-cup OpenOffice
+cup microsoftazurestorageexplorer
+cup Office365Business
+cup Picotorrent
 cup poshgit
+cup postman
 cup ReSharper
-cup Slack
 cup Spotify
 cup sql-server-management-studio
 cup steam
-cup sublimetext3
-cup VisualStudio2017Enterprise
+cup visualstudio2017-installer
 cup VisualStudioCode
 cup vlc
 cup vscode-cake
