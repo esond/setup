9bae564dd1e53584143da7eb7c6eb94177eef8c0
Eric Sondergard <4650644+esond@users.noreply.github.com>
2020-04-12 11:36:32 -0600

    Separate a 'home' chocolatey script to install entertainment tools

--- choco.ps1
+++ choco.ps1
@@ -1,52 +1,56 @@
-# Requires Execution-Policy to be set to Remote-Signed
+Function RequireAdmin {
+    If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+        Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -Verb RunAs
+        Exit
+    }
+}
 
-# Install Chocolatey (https://chocolatey.org/)
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/upgrade.ps1'))
+# Make sure we're in an admin shell, launch one if not
+RequireAdmin
+
+# Install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
+
+# Refresh the environment so choco commands are available
 refreshenv
+
+# Enable global confirmation for chocolatey packages.
+# Comment this out to approve every package individually.
 choco feature enable -n allowGlobalConfirmation
 
 # Install Chocolatey packages
-cup 1Password
-cup 7zip
-cup azure-cli
-cup adobereader
-cup baretail
-cup bulkrenameutility
-cup ConEmu
-cup DotNet4.6
-cup DotNet4.6.2
-cup dotnet4.7
-cup dotnet4.7.1
-cup dotnet4.7.2
-cup dotnetcore
-cup dotnetcore-sdk
-cup Firefox
-cup git
-cup GoogleChrome
-cup insomnia-rest-api-client
-cup iTunes
-cup linqpad5
-cup nordvpn
-cup nuget.commandline
-cup qbittorrent
-cup pdftk
-cup plexmediaserver
-cup poshgit
-cup ReSharper
-cup ripgrep
-cup sonarr
-cup Spotify
-cup sql-server-management-studio
-cup steam
-cup visualstuido-installer
-cup vlc
-cup vscode
-cup vscode-cake
-cup vscode-csharp
-cup vscode-csharpextensions
-cup vscode-gitattributes
-cup vscode-gitignore
-cup vscode-icons
-cup vscode-markdownlint
-cup vscode-mssql
-cup vscode-powershell
+choco upgrade `
+    7zip `
+    azure-cli `
+    adobereader `
+    baretail `
+    bulkrenameutility `
+    dotnetcore-sdk `
+    Firefox `
+    git `
+    GoogleChrome `
+    insomnia-rest-api-client `
+    linqpad5 `
+    microsoft-windows-terminal `
+    nuget.commandline `
+    pdftk `
+    poshgit `
+    ReSharper `
+    ripgrep `
+    Spotify `
+    sql-server-management-studio `
+    steam `
+    visualstuido-installer `
+    vlc `
+    vscode `
+    vscode-cake `
+    vscode-csharp `
+    vscode-csharpextensions `
+    vscode-gitattributes `
+    vscode-gitignore `
+    vscode-icons `
+    vscode-markdownlint `
+    vscode-mssql `
+    vscode-powershell `
